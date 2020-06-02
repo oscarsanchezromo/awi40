81aed3fe797c96cc33ce6d739d00f8ef16e2f50e
oscarsanchezrom <1718110860@utectulancingo.edu.mx>
2020-06-02 23:40:38 +0000

    listo funciona otra vez

--- semana_3/mvc/controllers/visitas.py
+++ semana_3/mvc/controllers/visitas.py
@@ -1,13 +1,18 @@
 import web
+from datetime import datetime
 
 class Visitas:
     def GET(self, nombre):
         try:
             cookie = web.cookies()
             visitas = "0"
+            now = datetime.now()
+
             print(cookie)
+
             if nombre:
                 web.setcookie("nombre",nombre,expires="", domain=None)
+
             else:
                 nombre = "NA"
                 web.setcookie("nombre",nombre,expires="", domain=None)
@@ -16,10 +21,11 @@ class Visitas:
                 visitas = int(cookie.get("visitas"))
                 visitas += 1
                 web.setcookie("visitas", str(visitas), expires="", domain=None)
+
             else:
                 web.setcookie("visitas", str(1), expires="", domain=None)
                 visitas = "1"
 
-            return "Visitas " + str(visitas) + "nombre " + nombre
+            return "Visitas " + str(visitas) + "nombre " + nombre + " fecha de la visita " + str(now)
         except Exception as e:
             return "Error " + str(e.args)
